b2e312b0217c2db736cc1b54f6811b3f8f76b5d1
Ania <44368856+aniagaska@users.noreply.github.com>
2025-04-27 10:44:43 -0400

    Add js

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-// How do you do this
+// Chris made the random image example https://codepen.io/bronzehedwick/pen/dyLdJmG 
+// Load random image from images folder on load
+(function main() {
+    'use strict';
+  
+    window.addEventListener('load', function handleLoad() {
+      const imagePaths = [
+        '/images/vet-1.png',
+        '/images/vet-2.png',
+        '/images/vet-3.png',
+      ];
+  
+      const randomImagePath = imagePaths[Math.floor(Math.random() * imagePaths.length)];
+      const imageContainer = document.getElementById('image-container');
+      const randomImage = new Image();
+      randomImage.src = randomImagePath;
+      randomImage.alt = 'Fern the dog is very cute';
+      imageContainer.appendChild(randomImage);
+    });
+  })();
